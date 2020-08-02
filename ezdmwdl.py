e570b9af0e28c0e509864fb4bca5028bbcc9087e
Yilin Hu <hu10xiaorui@outlook.com>
2020-08-01 21:11:30 -0700

    使用浏览器来下载番剧 改进了番剧识别

--- ezdmwdl.py
+++ ezdmwdl.py
@@ -1,27 +1,48 @@
 import os
 import platform
 import re
-import wget
+import webbrowser
 if platform.system() == "Windows":
     cpt = os.popen('''chcp''').read()
     cp = re.sub('[^0-9]','', cpt)
     os.system("chcp 936")
-vidl = input("输入第一话视频链接：")
+vidl = input("输入番剧链接：")
 hv = int(input("输入集数："))
-vids = vidl.find("html5/")
-vide = vidl.find(".html")
-vid = vidl[vids+6:vide]
-print("视频id：",vid)
-for v in range(hv):
+browser = input("使用浏览器下载？（y/n）：").lower()
+# vids = vidl.find("html5/")
+# vide = vidl.find(".html")
+# vid = vidl[vids+6:vide]
+# print("视频id：",vid)
+videourll=[]
+vp = os.popen('''curl --keepalive-time 10  --retry-delay 3 --retry-max-time 0  --retry 100 "'''+vidl+'''"''').read().replace("\n","")
+urllsp=vp.find("国内高速节点")
+urllep=vp.find('''<span class="1">1</span>''')
+urll=vp[urllsp+12:urllep]
+rurll=urll
+for x in range(hv):
+    a=rurll.find('''<a href="''')
+    b=rurll.find('''" >    	        <span class="''')
+    c=rurll.find('''</a>''')
+    videourll.append(rurll[a+len('''<a href="'''):b].replace('''"''',"").replace(">","").replace("\t","").replace(" ","").replace("/Home/Index/html5/","").replace(".html",""))
+    rurll=rurll[c+4:]
+
+i=0
+for video in videourll:
     print("==================================================================================================")
     print("下载视频网页中...")
-    vp = os.popen('''curl --keepalive-time 10 "https://api.tzdjzu.com/index.php?nk='''+str(int(vid)+v)+'''&barrage_switch="''').read()
+    vp = os.popen('''curl --keepalive-time 10  --retry-delay 3 --retry-max-time 0  --retry 100 "https://api.tzdjzu.com/index.php?nk='''+video+'''&barrage_switch="''').read()
     print("视频网页下载成功")
     dvp=vp.find('''<source src="''')
     dvep=vp.find('''" onerror="load_fail[0]()">''')
     vurl=vp[dvp+13:dvep]
     print("视频地址：",vp[dvp+13:dvep])
-    print("下载第"+str(v+1)+"集中...")
-    wget.download(vp[dvp+13:dvep], str(v+1)+'''.mp4 "''')
+    print("下载第"+str(hv-i)+"集中...")
+
+    if browser == "y":
+        webbrowser.open(vp[dvp+13:dvep])
+    else:
+        os.system('''curl --keepalive-time 10  --retry-delay 3 --retry-max-time 0 --retry 100 -o '''+str(hv-i)+'''.mp4 "'''+vp[dvp+13:dvep]+'''"''')
     print("视频下载成功")
-os.system("chcp "+cp)
+    i=i+1
+if platform.system() == "Windows":
+    os.system("chcp "+cp)
